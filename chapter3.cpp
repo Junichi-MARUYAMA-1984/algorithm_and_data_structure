a491350b47c951cabab9fa892ea71a1f4ce82070
Junichi MARUYAMA <jmaruyama.mbc@tmd.ac.jp>
2020-10-19 01:01:17 +0900

    章末問題3.6

--- chapter3.cpp
+++ chapter3.cpp
@@ -2,31 +2,23 @@
 #include <vector>
 using namespace std;
 
-int how_many_times(int N) {
-    int exp = 0; // 2で割り切れた回数
-    while (N % 2 == 0) {
-        N /= 2;
-        ++exp;
-    }
-    return exp;
-}
-
-// 無限大を表す値
-const int INF = 1234567890;
-
 int main() {
-    int N;
-    cin >> N;
+    int N, K;
+    cin >> N >> K;
 
-    vector<int> A(N);
-    for (int i = 0; i < N; ++i) {
-        cin >> A[i];
-    }
-
-    // A[N]各要素における「2で割り切れる回数」をそれぞれ求め、その中の最小値を求める。
-    int result = INF;
-    for (auto a : A) {
-        result = min(result, how_many_times(a));
+    int result = 0;
+    for (int i = 1; i < N; ++i) {
+        if (i > K) {
+            break;
+        }
+        for (int j = i; j < N - i; ++j) {
+            int diff = N - i - j;
+            if (diff <= K && diff >= i && diff >= j) {
+                ++result;
+            } else {
+                continue;
+            }
+        }
     }
 
     cout << result << endl;
