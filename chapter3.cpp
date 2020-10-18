7093be8f2dea14ea49323da101a26f1423bb6c14
Junichi MARUYAMA <jmaruyama.mbc@tmd.ac.jp>
2020-10-18 23:51:32 +0900

    code3.6 部分和問題に対するビットを用いる全探索解法

--- chapter3.cpp
+++ chapter3.cpp
@@ -2,37 +2,36 @@
 #include <vector>
 using namespace std;
 
-const int INF = 200000000; // 十分大きな値にする。
-
 int main() {
-    // 入力を受け取る
-    int N, K;
-    cin >> N >> K;
+    // 入力受け取り
+    int N, W;
+    cin >> N >> W;
 
-    vector<int> a(N), b(N);
+    vector<int> a(N);
     for (int i = 0; i < N; ++i) {
         cin >> a[i];
     }
-    for (int i = 0; i < N; ++i) {
-        cin >> b[i];
-    }
 
-    // 線型探索
-    int min_value = INF;
-    for (int i = 0; i < N; ++i) {
-        for (int j = 0; j < N; ++j) {
-            // 和がK未満の時は捨てる。
-            if (a[i] + b[j] < K) {
-                continue;
+    // bitは2^N通りの部分集合全体を動く。
+    bool exist = false; // Wに一致する部分和があるか否かのフラグ。
+    for (int bit = 0; bit < (1 << N); ++bit) {
+        int sum = 0; // 部分集合に含まれる要素の和
+        for (int i = 0; i < N; ++i) {
+            // i番目の要素a[i]が、bitで指定されている部分集合に含まれているかどうか。
+            // 含まれていたら和の計算に入れる。
+            if (bit & (1 << i)) {
+                sum += a[i];
             }
+        }
 
-            // 最小値を更新する
-            if (a[i] + b[j] < min_value) {
-                min_value = a[i] + b[j];
-            }
+        if (sum == W) {
+            exist = true;
         }
     }
 
-    // 結果出力
-    cout << min_value << endl;
+    if (exist) {
+        cout << "Yes" << endl;
+    } else {
+        cout << "No" << endl;
+    }
 }
