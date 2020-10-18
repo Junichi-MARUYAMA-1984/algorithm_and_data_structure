edd128578f81729fb5f62c9bdc89256090ec5be3
Junichi MARUYAMA <jmaruyama.mbc@tmd.ac.jp>
2020-10-18 23:37:49 +0900

    code3.4 ペア和の最小値を求める（K以上の範囲）

--- chapter3.cpp
+++ chapter3.cpp
@@ -6,19 +6,30 @@ const int INF = 200000000; // 十分大きな値にする。
 
 int main() {
     // 入力を受け取る
-    int N;
-    cin >> N;
+    int N, K;
+    cin >> N >> K;
 
-    vector<int> a(N);
+    vector<int> a(N), b(N);
     for (int i = 0; i < N; ++i) {
         cin >> a[i];
+    }
+    for (int i = 0; i < N; ++i) {
+        cin >> b[i];
     }
 
     // 線型探索
     int min_value = INF;
-    for (int i = 0; i < N; i++) {
-        if (a[i] < min_value) {
-            min_value = a[i];
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < N; ++j) {
+            // 和がK未満の時は捨てる。
+            if (a[i] + b[j] < K) {
+                continue;
+            }
+
+            // 最小値を更新する
+            if (a[i] + b[j] < min_value) {
+                min_value = a[i] + b[j];
+            }
         }
     }
 
