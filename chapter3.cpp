9ccbdcf1172ed66325abc6ddb028758ddd8b816f
Junichi MARUYAMA <jmaruyama.mbc@tmd.ac.jp>
2020-10-19 00:00:32 +0900

    章末問題3.3

--- chapter3.cpp
+++ chapter3.cpp
@@ -2,36 +2,30 @@
 #include <vector>
 using namespace std;
 
+const int INF = 200000000;
+
 int main() {
     // 入力受け取り
-    int N, W;
-    cin >> N >> W;
+    int N;
+    cin >> N;
 
     vector<int> a(N);
     for (int i = 0; i < N; ++i) {
         cin >> a[i];
     }
 
-    // bitは2^N通りの部分集合全体を動く。
-    bool exist = false; // Wに一致する部分和があるか否かのフラグ。
-    for (int bit = 0; bit < (1 << N); ++bit) {
-        int sum = 0; // 部分集合に含まれる要素の和
-        for (int i = 0; i < N; ++i) {
-            // i番目の要素a[i]が、bitで指定されている部分集合に含まれているかどうか。
-            // 含まれていたら和の計算に入れる。
-            if (bit & (1 << i)) {
-                sum += a[i];
-            }
+    int min_value = INF;
+    int min_second_value = INF;
+    for (int i = 0; i < N; ++i) {
+        if (a[i] < min_value) {
+            min_value = a[i];
         }
 
-        if (sum == W) {
-            exist = true;
+        if (a[i] > min_value && a[i] < min_second_value) {
+            min_second_value = a[i];
         }
     }
 
-    if (exist) {
-        cout << "Yes" << endl;
-    } else {
-        cout << "No" << endl;
-    }
+    cout << "Minimum value: " << min_value << endl;
+    cout << "Second minumum value: " << min_second_value << endl;
 }
