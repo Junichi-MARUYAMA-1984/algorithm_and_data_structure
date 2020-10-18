837c7ebbd69e30955968311d04e8638cb6e4d127
Junichi MARUYAMA <jmaruyama.mbc@tmd.ac.jp>
2020-10-18 23:26:49 +0900

    code3.2 特定の要素の存在する「添字」も取得する

--- chapter3.cpp
+++ chapter3.cpp
@@ -13,17 +13,14 @@ int main() {
     }
 
     // 線型探索
-    bool exist = false; // 初期値はfalseに。
+    int found_id = -1; // 初期値は-1などとあり得ない値に。
     for (int i = 0; i < N; ++i) {
         if (a[i] == v) {
-            exist = true; // 見つかったらフラグを立てる。
+            found_id = i; // 見つかったら添字を記録。
+            break; // ループを抜ける。
         }
     }
 
-    // 結果出力
-    if (exist) {
-        cout << "Yes" << endl;
-    } else {
-        cout << "No" << endl;
-    }
+    // 結果出力 (-1のときは見つからなかったことを意味する)
+    cout << found_id << endl;
 }
