335d7b45fc632fb0e07958a3a1cba5ec8c1edab7
Junichi MARUYAMA <jmaruyama.mbc@tmd.ac.jp>
2020-10-21 00:49:49 +0900

    code4.6 フィボナッチ数列を求める再帰関数の再帰呼び出しの様子

--- chapter4.cpp
+++ chapter4.cpp
@@ -2,17 +2,24 @@
 
 using namespace std;
 
-int GCD(int m, int n) {
+int fibo(int N) {
+    // 再帰関数を呼び出したことを報告する。
+    cout << "fibo(" << N << ") を呼び出しました。" << endl;
+
     // ベースケース
-    if (n == 0) {
-        return m;
+    if (N == 0) {
+        return 0;
+    } else if (N == 1) {
+        return 1;
     }
 
-    // 再帰呼び出し
-    return GCD(n, m % n);
+    // 再帰的に答えを求めて出力する
+    int result = fibo(N - 1) + fibo(N - 2);
+    cout << N << " 項目 = " << result << endl;
+
+    return result;
 }
 
 int main() {
-    cout << GCD(51, 15) << endl; // 3が出力される
-    cout << GCD(15, 51) << endl; // 3が出力される
+    fibo(15);
 }
