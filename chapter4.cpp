ad8bf84c708ce3db1619e70f6da9ebb7ac3f8e2c
Junichi MARUYAMA <jmaruyama.mbc@tmd.ac.jp>
2020-10-21 00:54:30 +0900

    code4.7 フィボナッチ数列をfor文による反復で求める

--- chapter4.cpp
+++ chapter4.cpp
@@ -1,25 +1,13 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-int fibo(int N) {
-    // 再帰関数を呼び出したことを報告する。
-    cout << "fibo(" << N << ") を呼び出しました。" << endl;
-
-    // ベースケース
-    if (N == 0) {
-        return 0;
-    } else if (N == 1) {
-        return 1;
-    }
-
-    // 再帰的に答えを求めて出力する
-    int result = fibo(N - 1) + fibo(N - 2);
-    cout << N << " 項目 = " << result << endl;
-
-    return result;
-}
-
 int main() {
-    fibo(15);
+    vector<long long> F(50);
+    F[0] = 0, F[1] = 1;
+    for (int N = 2; N < 50; ++N) {
+        F[N] = F[N - 1] + F[N - 2];
+        cout << N << " 項目: " << F[N] << endl;
+    }
 }
