139f8d20da596395f862104451dd819fed24246b
Junichi MARUYAMA <jmaruyama.mbc@tmd.ac.jp>
2020-10-21 02:48:23 +0900

    章末問題4.6　修正

--- chapter4.cpp
+++ chapter4.cpp
@@ -2,36 +2,29 @@
 #include <vector>
 using namespace std;
 
-vector<vector<bool>> memo;
+vector<vector<int>> memo;
 
-bool func(int i, int w, const vector<int>& a) {
+int func(int i, int w, const vector<int>& a) {
     // ベースケース
     if (i == 0) {
         if (w == 0) {
-            return memo.at(0).at(0) = true;
+            return memo.at(0).at(0) = 1;
         } else {
-            return false;
+            return memo.at(0).at(0) = 0;
         }
     }
 
-    // a[i - 1]を選ばない場合
-    if (memo.at(i - 1).at(w)) {
-        return memo.at(i).at(w) = true;
+    // メモ化変数をチェックし、計算済みなら返す。
+    if (memo.at(i).at(w) != -1) {
+        return memo.at(i).at(w);
     } else if (func(i - 1, w, a)) {
-        return memo.at(i).at(w) = true;
-    }
-
-    // a[i - 1]を選ぶ場合
-    if (w - a[i - 1] >= 0) {
-        if (memo.at(i - 1).at(w - a[i - 1])) {
-            return memo.at(i).at(w) = true;
-        } else if (func(i - 1, w - a[i - 1], a)) {
-            return memo.at(i).at(w) = true;
-        }
+        return memo.at(i).at(w) = 1;
+    } else if (func(i - 1, w - a[i - 1], a)) {
+        return memo.at(i).at(w) = 1;
     }
 
     // どちらもfalseの場合は、false
-    return false;
+    return 0;
 }
 
 int main() {
@@ -45,7 +38,7 @@ int main() {
     }
 
     // メモ化変数を初期化
-    memo.assign(N + 1, vector<bool>(W + 1, false));
+    memo.assign(N + 1, vector<int>(W + 1, -1));
 
     // 再帰的に解く
     if (func(N, W, a)) {
