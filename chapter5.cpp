42a34579ec6803195caca12070813b9cc7e6875e
Junichi MARUYAMA <jmaruyama.mbc@tmd.ac.jp>
2020-10-28 01:36:00 +0900

    code5.6 Frog問題を「メモ化再帰」で解く

--- chapter5.cpp
+++ chapter5.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 using namespace std;
 
-template<class T> void chmin(T& a, T b) {
+template<typename T> void chmin(T& a, T b) {
     if (a > b) {
         a = b;
     }
@@ -10,32 +10,48 @@ template<class T> void chmin(T& a, T b) {
 
 const long long INF = 1LL << 60; // 十分大きな値とする。ここでは2^60
 
-int main() {
-    // 入力
-    int N;
-    cin >> N;
+// 入力データと、メモ用のDPテーブル
+int N;
+vector<long long> h;
+vector<long long> dp;
 
-    vector<long long> h(N);
+long long rec(int i) {
+    // DPの値が更新されていたらそのままリターン。
+    if (dp[i] < INF) {
+        return dp[i];
+    }
+
+    // ベースケース。足場0のコストは0。
+    if (i == 0) {
+        return 0;
+    }
+
+    // 答えを表す変数をINFで初期化する。
+    long long res = INF;
+
+    // 足場i - 1から来た場合
+    chmin(res, rec(i - 1) + abs(h[i] - h[i - 1]));
+
+    // 足場i - 2から来た場合
+    if (i > 1) {
+        chmin(res, rec(i - 2) + abs(h[i] - h[i - 2]));
+    }
+
+    // 結果をメモ化しながら返す。
+    return dp[i] = res;
+}
+
+int main() {
+    // 入力受け取り
+    cin >> N;
+    h.resize(N);
     for (int i = 0; i < N; ++i) {
         cin >> h[i];
     }
 
-    // 初期化（最小化問題なのでINFに初期化する）
-    vector<long long> dp(N, INF);
-
-    // 初期条件
-    dp[0] = 0;
-
-    // ループ
-    for (int i = 0; i < N; ++i) {
-        if (i + 1 < N) {
-            chmin(dp[i + 1], dp[i] + abs(h[i] - h[i + 1]));
-        }
-        if (i + 2 < N) {
-            chmin(dp[i + 2], dp[i] + abs(h[i] - h[i + 2]));
-        }
-    }
+    // 初期化。最小化問題なのでINFで初期化する。
+    dp.assign(N, INF);
 
     // 答え
-    cout << dp[N - 1] << endl;
+    cout << rec(N - 1) << endl;
 }
