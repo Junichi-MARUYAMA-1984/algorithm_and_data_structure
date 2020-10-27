c8f5a8e0a3b61f55d84de88ad522c934a333ea3a
Junichi MARUYAMA <jmaruyama.mbc@tmd.ac.jp>
2020-10-28 01:15:05 +0900

    code5.4 Frog問題を「配る遷移形式」で解く

--- chapter5.cpp
+++ chapter5.cpp
@@ -20,17 +20,19 @@ int main() {
         cin >> h[i];
     }
 
-    // 初期化（最小化問題なので、INFに初期化する）
+    // 初期化（最小化問題なのでINFに初期化する）
     vector<long long> dp(N, INF);
 
     // 初期条件
     dp[0] = 0;
 
     // ループ
-    for (int i = 1; i < N; ++i) {
-        chmin(dp[i], dp[i - 1] + abs(h[i] - h[i - 1]));
-        if (i > 1) {
-            chmin(dp[i], dp[i - 2] + abs(h[i] - h[i - 2]));
+    for (int i = 0; i < N; ++i) {
+        if (i + 1 < N) {
+            chmin(dp[i + 1], dp[i] + abs(h[i] - h[i + 1]));
+        }
+        if (i + 2 < N) {
+            chmin(dp[i + 2], dp[i] + abs(h[i] - h[i + 2]));
         }
     }
 
